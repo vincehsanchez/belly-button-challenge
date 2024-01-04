3b46bd7cf98210a5a6590b2e24f41a2febf7935f
vincehsanchez <vincehsanchez@vincehsanchezs-MacBook-Air.local>
2024-01-04 11:37:38 -0500

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,57 +10,65 @@
 ///let labels = Object.keys(data.australia);
 
 // lets use D3 library to read in "samples.json"
+// but why do we have samples.json in the first place if we are using URL?
 function init() {
-  let data = [{
-    values: australia,
-    labels: labels,
-    type: "pie"
-  }];
+    var bb_samples_url = "";
+d3.json(bb_samples_url, function (json) {
+    //https://stackoverflow.com/questions/22325819/d3-js-get-json-from-url
+});
+  //let data = [{
+    //values: australia,
+    //labels: labels,
+    //type: "pie"
+  //}];
 
-  let layout = {
-    height: 600,
-    width: 800
-  };
+  //let layout = {
+    //height: 600,
+    //width: 800
+  //};
 
-  Plotly.newPlot("pie", data, layout);
-}
+  //Plotly.newPlot("pie", data, layout);
+//}
 
 // On change to the DOM, call getData()
 d3.selectAll("#selDataset").on("change", getData);
 
-// Function called by DOM changes
-function getData() {
-  let dropdownMenu = d3.select("#selDataset");
+}
+////here i think we can make the "horizontal bar chart with a dropdown menu...
+//... to display the top 10 OTUs found in that individual" 
+//// Function called by DOM changes
+//function getData() {
+ // let dropdownMenu = d3.select("#selDataset");
   // Assign the value of the dropdown menu option to a letiable
-  let dataset = dropdownMenu.property("value");
-  // Initialize an empty array for the country's data
-  let data = [];
+  //let dataset = dropdownMenu.property("value");
+  //// Initialize an empty array for the country's data
+  //let data = [];
 
-  if (dataset == 'australia') {
-      data = australia;
-  }
-  else if (dataset == 'brazil') {
-      data = brazil;
-  }
-  else if (dataset == 'uk') {
-      data = uk;
-  }
-  else if (dataset == 'mexico') {
-    data = mexico;
-  }
-  else if (dataset == 'singapore') {
-      data = singapore;
-  }
-  else if (dataset == 'southAfrica') {
-    data = southAfrica;
-  }
-// Call function to update the chart
-  updatePlotly(data);
-}
+  //if (dataset == 'australia') {
+    //  data = australia;
+  //}
+  //else if (dataset == 'brazil') {
+    //  data = brazil;
+  //}
+  //else if (dataset == 'uk') {
+      //data = uk;
+  //}
+  //else if (dataset == 'mexico') {
+   // data = mexico;
+  //}
+  //else if (dataset == 'singapore') {
+      //data = singapore;
+  //}
+  //else if (dataset == 'southAfrica') {
+    //data = southAfrica;
+  //}
+//// Call function to update the chart
+  //updatePlotly(data);
+//}
 
-// Update the restyled plot's values
-function updatePlotly(newdata) {
-  Plotly.restyle("pie", "values", [newdata]);
-}
+//// Update the restyled plot's values
+//function updatePlotly(newdata) {
+  //Plotly.restyle("pie", "values", [newdata]);
+//}
 
-init();
+//init();
