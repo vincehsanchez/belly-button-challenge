efcbed273ce2645492cfcdf1e611d352cc5e2fdf
vincehsanchez <vincehsanchez@vincehsanchezs-MacBook-Air.local>
2024-01-07 00:07:22 -0500

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,19 +1,8 @@
-// Create an array of each country's numbers
-//let australia = Object.values(data.australia);
-//let brazil = Object.values(data.brazil);
-//let uk = Object.values(data.uk);
-////let mexico = Object.values(data.mexico);
-//let singapore = Object.values(data.singapore);
-//let southAfrica = Object.values(data.southAfrica);
-
-// Create an array of category labels
-///let labels = Object.keys(data.australia);
-
 // lets use D3 library to read in "samples.json"
 // but why do we have samples.json in the first place if we are using URL?
 //BCS says that the json file is good practice and helpful if offline...
 const bb_samples = 
-"https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";function init() {
+"https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 // Promise Pending
 const dataPromise = d3.json(bb_samples);
 console.log("Data Promise: ", dataPromise);
@@ -21,11 +10,15 @@ console.log("Data Promise: ", dataPromise);
 d3.json(bb_samples).then(function(data) {
   console.log(data);
 });
+//data loaded, now we can make our chart
 
+///////////////////
+//fetch(bb_samples)
+ //   .then((response) => response.json())
+ //   .then((data) => {
 
-fetch(bb_samples)
-    .then((response) => response.json())
-    .then((data) => {
+///////////////////////
+
 //let data = [{
     //values: australia,
     //labels: labels,
@@ -39,20 +32,26 @@ fetch(bb_samples)
 
   //Plotly.newPlot("pie", data, layout);
 //}
-let barData = [{
-    x: Object.keys(venueCount),
-    y: Object.values(venueCount),
-    type: 'bar'
-  }];
 
-  Plotly.newPlot('myDivBar', barData, {
-    title: 'Top 10 OTUs in Individuals'
-  });
+////////////////////
+/////let barData = [{
+   // x: Object.keys(venueCount),
+   // y: Object.values(venueCount),
+   // type: 'bar'
+ // }];
 
+ // Plotly.newPlot('myDivBar', barData, {
+  //  title: 'Top 10 OTUs in Individuals'
+ // });
+////////////////////
+
+////////////////////
 // On change to the DOM, call getData()
-d3.selectAll("#selDataset").on("change", getData);
+//d3.selectAll("#selDataset").on("change", getData);
 
-}
+//}
+/////////////////////
+
 ////here i think we can make the "horizontal bar chart with a dropdown menu...
 //... to display the top 10 OTUs found in that individual" 
 //// Function called by DOM changes
