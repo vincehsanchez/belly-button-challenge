223d42b2a75938f5b7a0f4f5eea494051d8e2cd9
vincehsanchez <vincehsanchez@vincehsanchezs-MacBook-Air.local>
2024-01-20 22:03:28 -0500

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -108,9 +108,9 @@ function showUniqueSamples(selectedParticipantSample) {
   const bbSamples = d3.select("#bubble");
   bbSamples.html("");
   //no slicing, we want all
-  const bbSampleIDs = selectedParticipantSample.otu_ids;
-  const bbSampleValues = selectedParticipantSample.sample_values;
-  const bbSampleLabels = selectedParticipantSample.otu_labels;
+  const bbSampleIDs = selectedParticipantSample.otu_ids;//xvalues and marker colors
+  const bbSampleValues = selectedParticipantSample.sample_values;//yvalues and marker size
+  const bbSampleLabels = selectedParticipantSample.otu_labels;//text values only
   //here we want to modify x-alues and not have to jump between desc and asce (hope)..worked!
   console.log(top10SampleValues); //works!
   console.log(top10SampleIDs);
@@ -143,89 +143,4 @@ console.log(bbParticipants)
 //const selectedParticipantSample = data.samples.filter(sampleObj => sampleObj.id == selectedParticipantSample);
 //console.log(selectedParticipantSample);
 });
-//function countUniqueSamples(bbSamplesArray) {
- // const sampleValueCount = {};
- // bbSamplesArray.sample_values.forEach(sample_values =>{
-    //for now we will assume we are counting unique sample values
- //   if (countUniqueSamples[])
- // })
- // return sampleIds.age < 30;
-//}
-//function letsGetEachArray(sample) {
-//d3.json(bbSamples).then((data) => { //vscode expected "=>"
-//let bbResultArrays = data.samples.filter(sampleObj => sampleObj.id == sample); //sugggested by BCS
-  //^^"sample" not defined for some reason...^^..maybe needs a list or function first!
-//let bbResults = bbResultArrays[0];//suggested by BCS
-//console.log(bbResults);//suggested by BCS
-  //});//d3 brace and "then" parenthesis
-//};//for lgea function
-//lets get some charts going
-//function bellyButtonCharts(chosenParticipant) {
-  //const bb
-//}//for function bbcharts
-//let bbResultArrays = data.samples.filter(sampleObj => sampleObj.id <= 1601); //sugggested by BCS
-//let bbResults = bbResultArrays;//suggested by BCS
-//console.log(bbResults);//suggested by BCS
-////////////////////^^bbSamplesArray already does...^^
 
-
-
-//fetch(bb_samples)
-  // .then((response) => response.json())
-   //.then((data) => {
-
-////////////////////
-/////let barData = [{
-   // x: Object.keys(venueCount),
-   // y: Object.values(venueCount),
-   // type: 'bar'
- // }];
-
- // Plotly.newPlot('myDivBar', barData, {
-  //  title: 'Top 10 OTUs in Individuals'
- // });
-////////////////////
-
-////////////////////
-// On change to the DOM, call getData()
-//d3.selectAll("#selDataset").on("change", getData);
-
-//}
-/////////////////////
-
-////here i think we can make the "horizontal bar chart with a dropdown menu...
-//... to display the top 10 OTUs found in that individual" 
-//// Function called by DOM changes
-//function getData() {
- // let dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a letiable
-  //let dataset = dropdownMenu.property("value");
-  //// Initialize an empty array for the country's data
-  //let data = [];
-
-  //if (dataset == 'australia') {
-    //  data = australia;
-  //}
-  //else if (dataset == 'brazil') {
-    //  data = brazil;
-  //}
-  //else if (dataset == 'uk') {
-      //data = uk;
-  //}
-  //else if (dataset == 'mexico') {
-   // data = mexico;
-  //}
-  //else if (dataset == 'singapore') {
-      //data = singapore;
-  //}
-  //else if (dataset == 'southAfrica') {
-    //data = southAfrica;
-  //}
-//// Call function to update the chart
-  //updatePlotly(data);
-//}
-
-//// Update the restyled plot's values
-//function updatePlotly(newdata) {
-  //Plotly.restyle("pie", "values", [newdata]);
-//}
